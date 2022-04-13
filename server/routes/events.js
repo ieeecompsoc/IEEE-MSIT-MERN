f2b2f07012b801494308600be8623aa9c613c89d
Sahil <aggarwalsahil2004@gmail.com>
2022-04-13 20:51:02 +0530

    Event endpoints created

---
+++ server/routes/events.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+router.get('/get', async (req, res) => {
+    const events = await Event.find()
+    res.json(events)
+})
+
+router.post('/add', async (req, res) => {
+    try {
+        const { event_title, event_description, image, event_date } = req.body
+        const event = await Event.create({ event_title, event_description, image, event_date })
+        res.json(event)
+    } catch {
+        res.status(500).json({ error: 'Some Error Occured' })
+    }
+})
+
+router.put('/update/:id', async (req, res) => {
+    try {
+        const { event_title, event_description, image, event_date } = req.body
+        const event = await Event.findById(req.params.id)
+        if (!event) return res.status(404).json({ error: "Event not found!" })
+        const newEvent = await Event.findByIdAndUpdate(req.params.id, { $set: { event_title, event_description, image, event_date } }, { new: true })
+        res.json(newEvent)
+    } catch {
+        res.status(500).json({ error: 'Some Error Occured' })
+    }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id)
+        if (!event) return res.status(404).json({ error: "Event not found!" })
+        await Event.findByIdAndDelete(req.params.id)
+        res.json({ success: true })
+    } catch {
+        res.status(500).json({ error: 'Some Error Occured' })
+    }
+})
+
+module.exports = router;
