9dbbd280fb2dd5c9dc681b5809dfadebde93e57a
Im-Abhi <abhishekgupta942001@gmail.com>
2022-05-15 21:28:45 +0530

    minor change and add events

---
+++ client/src/pages/events/Events.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import './events.css';
+
+const Events = () => {
+    return (
+        <div className="events__container">
+
+        </div>
+    )
+}
+
+export default Events;
