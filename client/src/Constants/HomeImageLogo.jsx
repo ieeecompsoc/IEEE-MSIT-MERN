b937a6d91c0022685b30682b4ddd26b8b0b41b5b
Im-Abhi <abhishekgupta942001@gmail.com>
2022-06-09 20:24:54 +0530

    add image links

--- client/src/Constants/HomeImageLogo.jsx
+++ client/src/Constants/HomeImageLogo.jsx
@@ -3,23 +3,28 @@ import Images from "./Images";
 const logoImages = [
     {
         source: Images.logoMainBlue,
-        name: "IEEE Main"
+        name: "IEEE Main",
+        url: "/"
     },
     {
         source: Images.cs,
-        name: "IEEE CS"
+        name: "IEEE CS",
+        url: "/chapters/cs"
     },
     {
         source: Images.wie,
-        name: "IEEE WIE"
+        name: "IEEE WIE",
+        url: "/affinityGroup/wie"
     },
     {
         source: Images.pes,
-        name: "IEEE PES"
+        name: "IEEE PES",
+        url: "/chapters/pes"
     },
     {
         source: Images.ras,
-        name: "IEEE RAS"
+        name: "IEEE RAS",
+        url: "/chapters/ras"
     }
 ]
 
