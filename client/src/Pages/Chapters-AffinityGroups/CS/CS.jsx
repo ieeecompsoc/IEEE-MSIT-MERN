5d24f481bd1d05520f9e17cb92ed299e5bcd5c59
Im-Abhi <abhishekgupta942001@gmail.com>
2022-06-20 21:39:53 +0530

    Update chapters and affinity group pages

--- client/src/Pages/Chapters-AffinityGroups/CS/CS.jsx
+++ client/src/Pages/Chapters-AffinityGroups/CS/CS.jsx
@@ -1,33 +1,30 @@
-import react from 'react';
+// import react from 'react';
 import './CS.css';
 import CSLogo from '../../../Assets/computerSocietyLogo.svg';
+import Menubar from '../../../Components/Navbar/Menubar';
+
 const CS = () => {
     return (
         <div className="compsoc__chapter">
-            <div class="cs_navbar">
-            <a class="navbar-link" href="">Home</a>
-            <a class="navbar-link about" href="">About Us</a>
-            <a class="navbar-link" href="">Events</a>
-            <a class="navbar-link" href="">Executive Board</a>
-        </div>
-        <div class="contents">
-            <img class="CS-img" src={CSLogo} />
-            <h2 class="heading">Computer Society</h2>
-            <hr class="cs_line" />
-            <p class="cs_main-content">IEEE MSIT has had a pivotal role to play in advancing technological pursuits and social networking of MSIT. Since it's inception in 2009, it has played a forward role in providing it's members a platform to interact, collaborate and work towards a building a technically sound ecosystem. IEEE MSIT WIE, PES, MTTS, CS chapters have worked in cohesion to explore and bring forth various engineering avenues and opportunities such as hackathons, seminars, hands-on workshops, industrial visits, women empowerment seminars, technical project exhibitions, STEP programmes and a lot more.
-                <br />
-                <br />
-                The latest IEEE MSIT advent is it's AWP-PCB lab which is a project centric lab aiming at finding hardware solutions to real world glitches faced by the institution as a whole.
-                <br />
-                <br />
-                IEEE MSIT plans to keep growing, directing, mentoring and spontaneously nurturing the budding technologists of the institution.
-                <br />
-                <br />
-                With TPE as it's major project exhibitory event, IEEE MSIT focusses on hardware and software solutions and provides a ready platform for the budding engineers across the country to exhibit their engineering prowess and put their skills to a judicious use. The exhibits are judged by a panel of experienced judges who serve as able mentors to the students.
-                <br />
-                <br />
-                With it's wide reach, IEEE MSIT serves as an ideal platform for the students to network, co-learn and develop.</p>
-        </div>
+            <Menubar backgroundClr="#FFA300" />
+            <div class="contents">
+                <img class="CS-img" src={CSLogo} alt="cs-logo"/>
+                <h2 class="heading">Computer Society</h2>
+                <hr class="cs_line" />
+                <p class="cs_main-content">IEEE MSIT has had a pivotal role to play in advancing technological pursuits and social networking of MSIT. Since it's inception in 2009, it has played a forward role in providing it's members a platform to interact, collaborate and work towards a building a technically sound ecosystem. IEEE MSIT WIE, PES, MTTS, CS chapters have worked in cohesion to explore and bring forth various engineering avenues and opportunities such as hackathons, seminars, hands-on workshops, industrial visits, women empowerment seminars, technical project exhibitions, STEP programmes and a lot more.
+                    <br />
+                    <br />
+                    The latest IEEE MSIT advent is it's AWP-PCB lab which is a project centric lab aiming at finding hardware solutions to real world glitches faced by the institution as a whole.
+                    <br />
+                    <br />
+                    IEEE MSIT plans to keep growing, directing, mentoring and spontaneously nurturing the budding technologists of the institution.
+                    <br />
+                    <br />
+                    With TPE as it's major project exhibitory event, IEEE MSIT focusses on hardware and software solutions and provides a ready platform for the budding engineers across the country to exhibit their engineering prowess and put their skills to a judicious use. The exhibits are judged by a panel of experienced judges who serve as able mentors to the students.
+                    <br />
+                    <br />
+                    With it's wide reach, IEEE MSIT serves as an ideal platform for the students to network, co-learn and develop.</p>
+            </div>
         </div>
     )
 }
--- client/src/Pages/Chapters-AffinityGroups/RAS/RAS.jsx
+++ client/src/Pages/Chapters-AffinityGroups/RAS/RAS.jsx
@@ -1,35 +1,32 @@
-import react from 'react';
+// import react from 'react';
 import './RAS.css';
 import RASLogo from '../../../Assets/RASLogo.svg';
+import Menubar from '../../../Components/Navbar/Menubar';
+
 const RAS = () => {
     return (
         <div className="ras__chapter">
-                <div class="ras_navbar">
-                    <a class="navbar-link" href="">Home</a>
-                    <a class="navbar-link about" href="">About Us</a>
-                    <a class="navbar-link" href="">Events</a>
-                    <a class="navbar-link" href="">Executive Board</a>
-                </div>
-                <div class="contents">
-                    <img class="RAS-img" src={RASLogo}/>
-                        <h2 class="heading">Robotics & Automation Society</h2>
-                        <hr class="ras_line"/>
-                            <p class="ras_main-content">IEEE MSIT has had a pivotal role to play in advancing technological pursuits and social networking of MSIT. Since it's inception in 2009, it has played a forward role in providing it's members a platform to interact, collaborate and work towards a building a technically sound ecosystem. IEEE MSIT WIE, PES, MTTS, CS chapters have worked in cohesion to explore and bring forth various engineering avenues and opportunities such as hackathons, seminars, hands-on workshops, industrial visits, women empowerment seminars, technical project exhibitions, STEP programmes and a lot more.
-                <br />
-                                <br />
-                                The latest IEEE MSIT advent is it's AWP-PCB lab which is a project centric lab aiming at finding hardware solutions to real world glitches faced by the institution as a whole.
-                <br />
-                                <br />
-                                IEEE MSIT plans to keep growing, directing, mentoring and spontaneously nurturing the budding technologists of the institution.
-                <br />
-                                <br />
-                                With TPE as it's major project exhibitory event, IEEE MSIT focusses on hardware and software solutions and provides a ready platform for the budding engineers across the country to exhibit their engineering prowess and put their skills to a judicious use. The exhibits are judged by a panel of experienced judges who serve as able mentors to the students.
-                <br />
-                                <br />
-                                With it's wide reach, IEEE MSIT serves as an ideal platform for the students to network, co-learn and develop.</p>
+            <Menubar backgroundClr="#BA0C2F" />
+            <div class="contents">
+                <img class="RAS-img" src={RASLogo} alt="ras-logo"/>
+                <h2 class="heading">Robotics and Automation Society</h2>
+                <hr class="ras_line" />
+                <p class="ras_main-content">IEEE MSIT has had a pivotal role to play in advancing technological pursuits and social networking of MSIT. Since it's inception in 2009, it has played a forward role in providing it's members a platform to interact, collaborate and work towards a building a technically sound ecosystem. IEEE MSIT WIE, PES, MTTS, CS chapters have worked in cohesion to explore and bring forth various engineering avenues and opportunities such as hackathons, seminars, hands-on workshops, industrial visits, women empowerment seminars, technical project exhibitions, STEP programmes and a lot more.
+                    <br />
+                    <br />
+                    The latest IEEE MSIT advent is it's AWP-PCB lab which is a project centric lab aiming at finding hardware solutions to real world glitches faced by the institution as a whole.
+                    <br />
+                    <br />
+                    IEEE MSIT plans to keep growing, directing, mentoring and spontaneously nurturing the budding technologists of the institution.
+                    <br />
+                    <br />
+                    With TPE as it's major project exhibitory event, IEEE MSIT focusses on hardware and software solutions and provides a ready platform for the budding engineers across the country to exhibit their engineering prowess and put their skills to a judicious use. The exhibits are judged by a panel of experienced judges who serve as able mentors to the students.
+                    <br />
+                    <br />
+                    With it's wide reach, IEEE MSIT serves as an ideal platform for the students to network, co-learn and develop.</p>
             </div>
         </div>
-                    )
-                }
-                
+    )
+}
+
 export default RAS;
