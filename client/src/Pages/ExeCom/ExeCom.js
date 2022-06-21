af9b3b8f975406bd019130fd95d8f6ed643bec74
Bobby Kumar <kashyapashish0002@gmail.com>
2022-06-21 18:52:50 +0530

    Execom page responsiveness added

--- client/src/Pages/ExeCom/ExeCom.js
+++ client/src/Pages/ExeCom/ExeCom.js
@@ -2,72 +2,10 @@ import React from 'react';
 import './ExeCom.css';
 import Menubar from '../../Components/Navbar/Menubar'
 
+import execomDetailsOptions from '../../Constants/ExecomDetailsOptions';
 import { AiOutlineMail, AiOutlineLinkedin, AiOutlineInstagram,AiOutlineFile } from 'react-icons/ai';
 
 const team = require("./ExeComDetails.json");
-
-const execomDetails = [
-    {
-        execom_section: "ta",
-        section_heading: "Teacher Advisor",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "main",
-        section_heading: "IEEE MAIN",
-        heading_color: "#00629B"
-    },
-    {
-        execom_section: "ras",
-        section_heading: "IEEE RAS",
-        heading_color: "#BA0C2F"
-    },
-    {
-        execom_section: "cs",
-        section_heading: "IEEE CS",
-        heading_color: "#FFA300"
-    },
-    {
-        execom_section: "pes",
-        section_heading: "IEEE PES",
-        heading_color: "#00843D"
-    },
-    {
-        execom_section: "wie",
-        section_heading: "IEEE WIE",
-        heading_color: "#981D97"
-    },
-    {
-        execom_section: "A",
-        section_heading: "Web Development Committee",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "B",
-        section_heading: "Publicity Committee",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "C",
-        section_heading: "Program Committee",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "D",
-        section_heading: "Project Committee",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "E",
-        section_heading: "Editorial Committee",
-        heading_color: "#000000"
-    },
-    {
-        execom_section: "F",
-        section_heading: "Design Committee",
-        heading_color: "#000000"
-    }
-]
 
 const RenderExecomSection = ({ list, details }) => {
     const ranks = list.map(objects => {
@@ -216,25 +154,18 @@ const RenderExecomSection = ({ list, details }) => {
 const ExeCom = () => {
     return (
         <section className="app__execom">
-            {/* <Menubar backgroundClr="#009CA6" /> */}
+            <Menubar backgroundClr="#009CA6" />
             <div className="app__execom-mainContainer">
                 <div className="app__execom-headerContainer">
                     <h1 className="app__execom-header">Meet the Execom 2022-23</h1>
                     <div className="hr-header"></div>
                 </div>
                 <div className="app__execom-execomListContainer">
-                    {
-                        Object.keys(team).map ((key, index) => {
-                            return (
-                                (team[key].length) && (
-                                    <RenderExecomSection
-                                    list={team[key]}
-                                    details={execomDetails[index]}
-                                />
-                                )
-                            )
-                        })
-                    }
+                    {Object.keys(team).map((key, index) => {
+                        return (
+                            (team[key].length) ? (<RenderExecomSection list={team[key]} details={execomDetailsOptions[index]} />) : (<></>)
+                        )
+                    })}
                 </div>
             </div>
         </section>
