c14def598ab8445b5d59a9d0afba5029660ce5f2
Im-Abhi <abhishekgupta942001@gmail.com>
2022-07-29 22:15:22 +0530

    Add login page

--- client/htmlFiles/readme.md
+++
@@ -1,1 +0,0 @@
-Upload the html css files here by creating a new folder with the given name ex --> events, blogs.
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App tab="home" />
+  </React.StrictMode>);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
